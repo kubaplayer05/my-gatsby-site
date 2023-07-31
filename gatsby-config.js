5fdd928d87183d3ef2b63d1debc032be4e9eb561
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-07-31 13:39:15 +0200

    finish part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,12 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
+    siteMetadata: {
+        title: `My Gatsby Site`,
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+    ],
 }
